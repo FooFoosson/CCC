be877b2f77469a81af65ce2f44d6e4c2030f4b7a
Aleksandar Radovic <aleksandar.radovic@partner.bmw.de>
2024-02-24 09:32:47 +0100

    Parser changes
    fixed SymbolTable::lookup and SyntaxTree::parse methods,
    added method to print the syntax tree,
    added method to convert the syntax tree to an ast,
    minor code improvements

--- src/include/parser.h
+++ src/include/parser.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "lexer.h"
+#include <deque>
 #include <stack>
 #include <unordered_map>
+#include <vector>
 
 namespace ccc {
 
@@ -17,17 +19,17 @@ enum class Type {
     STRING_PTR,
 };
 
-enum class Scope {
+enum class StorageSpecifier {
     STATIC,
     EXTERN,
     AUTO
 };
 
 struct Symbol {
-    Symbol(Type type, Scope scope);
+    Symbol(Type type, StorageSpecifier storageSpecifier);
 
     Type type;
-    Scope scope;
+    StorageSpecifier storageSpecifier;
 };
 
 class SymbolTable {
@@ -35,8 +37,8 @@ public:
     SymbolTable();
     ~SymbolTable();
 
-    bool lookup(std::string& symbolName, Symbol& outSymbol);
-    bool insert(std::string& symbolName, Symbol& symbol);
+    bool lookup(const std::string& symbolName, Symbol& outSymbol);
+    bool insert(const std::string& symbolName, const Symbol& symbol);
     void addScope();
     void removeScope();
 
@@ -51,20 +53,24 @@ public:
     ~SyntaxTree();
 
     bool insert(const std::string& parentNonTerminal, const std::vector<Token>& tokens);
+    void printSyntaxTree();
 
-    class SyntaxTreeNode {
-    public:
+    struct SyntaxTreeNode {
         SyntaxTreeNode(Token val);
-        ~SyntaxTreeNode();
+        virtual ~SyntaxTreeNode();
 
         Token val;
         SyntaxTreeNode* next;
         SyntaxTreeNode* children;
     };
+    struct FunctionNode : SyntaxTreeNode {
+    };
+
     SyntaxTreeNode* root;
 
 private:
-    std::unordered_map<std::string, std::stack<SyntaxTreeNode*>> nonTerminalsToNodes;
+    void levelOrderTraversal(SyntaxTreeNode* node, unsigned int level, std::vector<std::vector<SyntaxTreeNode*>>& levels);
+    std::unordered_map<std::string, std::deque<SyntaxTreeNode*>> nonTerminalsToNodes;
 };
 
 /* Each compilation unit should have their own Parser instance */
@@ -72,27 +78,32 @@ class Parser {
 public:
     virtual ~Parser() = default;
 
-    virtual bool parsingAlgorithm(SyntaxTree& res) = 0;
+    virtual bool parse(SyntaxTree& res) = 0;
+    virtual void convertToAst(SyntaxTree& st) = 0;
 
 protected:
-    Parser(std::queue<Token*>& sharedBuffer);
+    Parser(SharedBuffer& buffer);
 
-    /* void addToSymbolTable(Token& token); */
+    void addToSymbolTable(Token& token, Type type);
     SymbolTable symbolTable;
-    std::queue<Token*>& sharedBuffer;
+    SharedBuffer& buffer;
     /* Maps non-terminals to a map which maps terminals to productions */
     std::unordered_map<std::string, std::unordered_map<Terminal, std::vector<std::string>>> parsingTable;
     std::stack<std::string> grammarSymbols;
     std::unordered_set<std::string> terminals;
+    const static std::unordered_map<Terminal, std::string> terminalsToProductions;
+    const static std::unordered_map<std::string, Terminal> productionsToTerminals;
 };
 
 class LL1Parser : public Parser {
 public:
-    LL1Parser(std::queue<Token*>& sharedBuffer);
+    LL1Parser(SharedBuffer& buffer);
 
-    bool parsingAlgorithm(SyntaxTree& res) override;
+    bool parse(SyntaxTree& res) override;
+    void convertToAst(SyntaxTree& st) override;
 
 private:
+    SyntaxTree::SyntaxTreeNode* convertToAstHelper(SyntaxTree::SyntaxTreeNode* node);
     void continueGrammarMatching();
     void expandProduction(SyntaxTree& st);
 };
--- src/parser.cpp
+++ src/parser.cpp
@@ -1,10 +1,14 @@
 #include "parser.h"
 
+#include <algorithm>
+#include <cstddef>
 #include <iostream>
-#include <queue>
-#include <unordered_map>
 
-#include "include/parser.h"
+ccc::Symbol::Symbol(Type type, StorageSpecifier storageSpecifier)
+    : type { type }
+    , storageSpecifier { storageSpecifier }
+{
+}
 
 ccc::SymbolTable::SymbolTable()
 {
@@ -28,7 +32,7 @@ void ccc::SymbolTable::removeScope()
     symbols.pop();
 }
 
-bool ccc::SymbolTable::insert(std::string& symbolName, Symbol& symbol)
+bool ccc::SymbolTable::insert(const std::string& symbolName, const Symbol& symbol)
 {
     std::unordered_map<std::string, Symbol>* currentTable = symbols.top();
     if (currentTable->find(symbolName) != currentTable->end())
@@ -37,7 +41,7 @@ bool ccc::SymbolTable::insert(std::string& symbolName, Symbol& symbol)
     return true;
 }
 
-bool ccc::SymbolTable::lookup(std::string& symbolName, Symbol& outSymbol)
+bool ccc::SymbolTable::lookup(const std::string& symbolName, Symbol& outSymbol)
 {
     while (!symbols.empty()) {
         std::unordered_map<std::string, Symbol>* currentTable = symbols.top();
@@ -76,37 +80,48 @@ ccc::SyntaxTree::SyntaxTreeNode::SyntaxTreeNode(Token val)
 
 bool ccc::SyntaxTree::insert(const std::string& parentNonTerminal, const std::vector<Token>& tokens)
 {
-    if (!parentNonTerminal.size()) {
+    if (parentNonTerminal.empty()) {
         root = new SyntaxTreeNode(tokens[0]);
-        nonTerminalsToNodes[tokens[0].lexeme].push(root);
+        nonTerminalsToNodes[tokens[0].lexeme].push_back(root);
         return true;
     }
-    if (nonTerminalsToNodes.find(parentNonTerminal) == nonTerminalsToNodes.end())
+    // E->SEE
+    // E->E
+    auto parentIterator = nonTerminalsToNodes.find(parentNonTerminal);
+    if (parentIterator == nonTerminalsToNodes.end())
         return false;
-    SyntaxTreeNode* parent = nonTerminalsToNodes[parentNonTerminal].top();
+    SyntaxTreeNode* parent = parentIterator->second.front();
     bool sameNonTerminalEncountered = false;
     // insert node as a child of the last inserted grammarSymbol node
     for (const Token& t : tokens) {
         SyntaxTreeNode* children;
-        if (t.lexeme == parentNonTerminal) {
-            nonTerminalsToNodes[parentNonTerminal].pop();
+        if (t.lexeme == parentNonTerminal && !sameNonTerminalEncountered) {
+            parentIterator->second.pop_front();
             sameNonTerminalEncountered = true;
         }
         if (parent->children == nullptr) {
             children = new SyntaxTreeNode(t);
             parent->children = children;
         } else {
+            children = parent->children;
             while (children->next != nullptr)
                 children = children->next;
             children->next = new SyntaxTreeNode(t);
             children = children->next;
         }
         // add the newly added node to the map
-        if (t.term == Terminal::NON_TERMINAL)
-            nonTerminalsToNodes[t.lexeme].push(children);
+        if (t.term == Terminal::NON_TERMINAL) {
+            if (parentIterator->second.empty())
+                nonTerminalsToNodes[t.lexeme].push_back(children);
+            else
+                nonTerminalsToNodes[t.lexeme].push_front(children);
+        }
     }
-    if (!sameNonTerminalEncountered)
-        nonTerminalsToNodes[parentNonTerminal].pop();
+    if (!sameNonTerminalEncountered) {
+        parentIterator->second.pop_front();
+        if (parentIterator->second.empty())
+            nonTerminalsToNodes.erase(parentIterator);
+    }
     return true;
 }
 
@@ -116,8 +131,41 @@ ccc::SyntaxTree::SyntaxTreeNode::~SyntaxTreeNode()
     delete children;
 }
 
-ccc::Parser::Parser(std::queue<Token*>& sharedBuffer)
-    : sharedBuffer(sharedBuffer)
+void ccc::SyntaxTree::printSyntaxTree()
+{
+    std::vector<std::vector<SyntaxTreeNode*>> levels;
+    levelOrderTraversal(root, 0, levels);    
+	for(auto& level:levels){
+		for(auto node:level){
+			std::cout<<node->val.lexeme;
+            unsigned int numChildren=0;
+            SyntaxTreeNode* child=node->children;
+            while(child!=nullptr){
+                ++numChildren;
+                child=child->next;
+            }
+            std::cout<<"("<<numChildren<<") ";
+        }
+		std::cout<<std::endl;
+	}
+}
+
+void ccc::SyntaxTree::levelOrderTraversal(SyntaxTreeNode* node, unsigned int level,std::vector<std::vector<SyntaxTreeNode*>>& levels)
+{
+	if(node==nullptr)
+		return;
+	if(levels.size()==level)
+		levels.push_back(std::vector<ccc::SyntaxTree::SyntaxTreeNode*>());
+	levels[level].push_back(node);
+	SyntaxTreeNode* child=node->children;
+	while(child){
+		levelOrderTraversal(child, level+1, levels);
+		child=child->next;
+	}
+}
+
+ccc::Parser::Parser(SharedBuffer& buffer)
+    : buffer{buffer}
 {
     grammarSymbols.push("$");
     grammarSymbols.push("E");
@@ -144,82 +192,151 @@ ccc::Parser::Parser(std::queue<Token*>& sharedBuffer)
     terminals.emplace(")");
 }
 
-ccc::LL1Parser::LL1Parser(std::queue<Token*>& sharedBuffer)
-    : Parser(sharedBuffer)
-{
-    parsingTable.emplace("E", std::unordered_map<Terminal, std::vector<std::string>>());
-    parsingTable["E"].emplace(Terminal::ID, std::vector<std::string> { "S", "E'" });
-    parsingTable["E"].emplace(Terminal::INT_LITERAL, std::vector<std::string> { "S", "E'" });
-    parsingTable["E"].emplace(Terminal::FLOAT_LITERAL, std::vector<std::string> { "S", "E'" });
-    parsingTable["E"].emplace(Terminal::OPENING_BRACKET, std::vector<std::string> { "S", "E'" });
+const std::unordered_map<ccc::Terminal, std::string> ccc::Parser::terminalsToProductions = {
+    { Terminal::ID, "id" },
+    { Terminal::BUILTIN_TYPE_INT, "int" },
+    { Terminal::BUILTIN_TYPE_FLOAT, "float" },
+    { Terminal::BUILTIN_TYPE_CHAR, "char" },
+    { Terminal::CONTROL_FLOW_BRANCH, "if" },
+    { Terminal::CONTROL_FLOW_WHILE, "while" },
+    { Terminal::ARITHMETIC_OP_PLUS, "+" },
+    { Terminal::ARITHMETIC_OP_MINUS, "-" },
+    { Terminal::ARITHMETIC_OP_MULT, "*" },
+    { Terminal::ARITHMETIC_OP_DIV, "/" },
+    { Terminal::LOGICAL_OP, "logop" },
+    { Terminal::ASSIGNMENT_OP, "=" },
+    { Terminal::INT_LITERAL, "int literal" },
+    { Terminal::FLOAT_LITERAL, "float literal" },
+    { Terminal::STRING_LITERAL, "string literal" },
+    { Terminal::SEMICOLON, ";" },
+    { Terminal::OPEN_SCOPE, "{" },
+    { Terminal::CLOSED_SCOPE, "}" },
+    { Terminal::OPENING_BRACKET, "(" },
+    { Terminal::CLOSING_BRACKET, ")" },
+    { Terminal::FILE_END, "eof" }
+};
 
-    parsingTable.emplace("S", std::unordered_map<Terminal, std::vector<std::string>>());
-    parsingTable["S"].emplace(Terminal::ARITHMETIC_OP_PLUS, std::vector<std::string> { "S", "E'", "+" });
-    parsingTable["S"].emplace(Terminal::ARITHMETIC_OP_MINUS, std::vector<std::string> { "S", "E'", "-" });
+const std::unordered_map<std::string, ccc::Terminal> ccc::Parser::productionsToTerminals = {
+    { "id", Terminal::ID },
+    { "int", Terminal::BUILTIN_TYPE_INT },
+    { "float", Terminal::BUILTIN_TYPE_FLOAT },
+    { "char", Terminal::BUILTIN_TYPE_CHAR },
+    { "if", Terminal::CONTROL_FLOW_BRANCH },
+    { "while", Terminal::CONTROL_FLOW_WHILE },
+    { "+", Terminal::ARITHMETIC_OP_PLUS },
+    { "-", Terminal::ARITHMETIC_OP_MINUS },
+    { "*", Terminal::ARITHMETIC_OP_MULT },
+    { "/", Terminal::ARITHMETIC_OP_DIV },
+    { "logop", Terminal::LOGICAL_OP },
+    { "=", Terminal::ASSIGNMENT_OP },
+    { "int literal", Terminal::INT_LITERAL },
+    { "float literal", Terminal::FLOAT_LITERAL },
+    { "string literal", Terminal::STRING_LITERAL },
+    { ";", Terminal::SEMICOLON },
+    { "{", Terminal::OPEN_SCOPE },
+    { "}", Terminal::CLOSED_SCOPE },
+    { "(", Terminal::OPENING_BRACKET },
+    { ")", Terminal::CLOSING_BRACKET },
+    { "eof", Terminal::FILE_END }
+};
+
+ccc::LL1Parser::LL1Parser(SharedBuffer& buffer)
+    : Parser{buffer}
+{
+    parsingTable.emplace("E", std::unordered_map<Terminal, std::vector<std::string>> {});
+    parsingTable["E"].emplace(Terminal::ID, std::vector<std::string> { "E'", "S" });
+    parsingTable["E"].emplace(Terminal::INT_LITERAL, std::vector<std::string> { "E'", "S" });
+    parsingTable["E"].emplace(Terminal::FLOAT_LITERAL, std::vector<std::string> { "E'", "S" });
+    parsingTable["E"].emplace(Terminal::OPENING_BRACKET, std::vector<std::string> { "E'", "S" });
+
+    parsingTable.emplace("S", std::unordered_map<Terminal, std::vector<std::string>> {});
+    parsingTable["S"].emplace(Terminal::ARITHMETIC_OP_PLUS, std::vector<std::string> { "+", "E'", "S" });
+    parsingTable["S"].emplace(Terminal::ARITHMETIC_OP_MINUS, std::vector<std::string> { "-", "E'", "S" });
     parsingTable["S"].emplace(Terminal::FILE_END, std::vector<std::string> { "epsilon" });
     parsingTable["S"].emplace(Terminal::CLOSING_BRACKET, std::vector<std::string> { "epsilon" });
 
-    parsingTable.emplace("E'", std::unordered_map<Terminal, std::vector<std::string>>());
-    parsingTable["E'"].emplace(Terminal::ID, std::vector<std::string> { "F", "T" });
-    parsingTable["E'"].emplace(Terminal::INT_LITERAL, std::vector<std::string> { "F", "T" });
-    parsingTable["E'"].emplace(Terminal::FLOAT_LITERAL, std::vector<std::string> { "F", "T" });
-    parsingTable["E'"].emplace(Terminal::OPENING_BRACKET, std::vector<std::string> { "F", "T" });
+    parsingTable.emplace("E'", std::unordered_map<Terminal, std::vector<std::string>> {});
+    parsingTable["E'"].emplace(Terminal::ID, std::vector<std::string> { "T", "F" });
+    parsingTable["E'"].emplace(Terminal::INT_LITERAL, std::vector<std::string> { "T", "F" });
+    parsingTable["E'"].emplace(Terminal::FLOAT_LITERAL, std::vector<std::string> { "T", "F" });
+    parsingTable["E'"].emplace(Terminal::OPENING_BRACKET, std::vector<std::string> { "T", "F" });
 
-    parsingTable.emplace("F", std::unordered_map<Terminal, std::vector<std::string>>());
-    parsingTable["F"].emplace(Terminal::ARITHMETIC_OP_MULT, std::vector<std::string> { "F", "T", "*" });
-    parsingTable["F"].emplace(Terminal::ARITHMETIC_OP_DIV, std::vector<std::string> { "F", "T", "/" });
+    parsingTable.emplace("F", std::unordered_map<Terminal, std::vector<std::string>> {});
+    parsingTable["F"].emplace(Terminal::ARITHMETIC_OP_MULT, std::vector<std::string> { "*", "T", "F" });
+    parsingTable["F"].emplace(Terminal::ARITHMETIC_OP_DIV, std::vector<std::string> { "/", "T", "F" });
     parsingTable["F"].emplace(Terminal::ARITHMETIC_OP_PLUS, std::vector<std::string> { "epsilon" });
     parsingTable["F"].emplace(Terminal::ARITHMETIC_OP_MINUS, std::vector<std::string> { "epsilon" });
     parsingTable["F"].emplace(Terminal::FILE_END, std::vector<std::string> { "epsilon" });
     parsingTable["F"].emplace(Terminal::CLOSING_BRACKET, std::vector<std::string> { "epsilon" });
 
-    parsingTable.emplace("T", std::unordered_map<Terminal, std::vector<std::string>>());
+    parsingTable.emplace("T", std::unordered_map<Terminal, std::vector<std::string>> {});
     parsingTable["T"].emplace(Terminal::ID, std::vector<std::string> { "id" });
     parsingTable["T"].emplace(Terminal::INT_LITERAL, std::vector<std::string> { "int literal" });
     parsingTable["T"].emplace(Terminal::FLOAT_LITERAL, std::vector<std::string> { "float literal" });
-    parsingTable["T"].emplace(Terminal::OPENING_BRACKET, std::vector<std::string> { ")", "E", "(" });
+    parsingTable["T"].emplace(Terminal::OPENING_BRACKET, std::vector<std::string> { "(", "E", ")" });
 }
 
-/* void ccc::Parser::addToSymbolTable(Token& token){ */
-/* 	if(token.term==Terminal::OPEN_SCOPE) */
-/* 		symbolTable.addScope(); */
-/* 	else if(token.term==Terminal::CLOSED_SCOPE) */
-/* 		symbolTables.closeScope(); */
-/* 	else if(token.term==Terminal::ID) */
-/* 		symbolTable.insert(, new Symbol(, )); */
-/* } */
+void ccc::Parser::addToSymbolTable(ccc::Token& token, ccc::Type type)
+{
+    if (token.term == Terminal::OPEN_SCOPE)
+        symbolTable.addScope();
+    else if (token.term == Terminal::CLOSED_SCOPE)
+        symbolTable.removeScope();
+    else if (token.term == Terminal::ID) {
+        Symbol symbol { type, StorageSpecifier::AUTO };
+        symbolTable.insert(token.lexeme, symbol);
+    }
+}
 
 void ccc::LL1Parser::expandProduction(SyntaxTree& st)
 {
-    Terminal inputTerm = sharedBuffer.front()->term;
+    Token* inputToken = buffer.consume();
     std::string currentGrammarSymbol = grammarSymbols.top();
     std::vector<Token> productionTokens;
 
     grammarSymbols.pop();
 
-    for (std::string& production : parsingTable[currentGrammarSymbol][inputTerm])
+    for (std::string& production : parsingTable[currentGrammarSymbol][inputToken->term])
         if (production != "epsilon") {
-            grammarSymbols.push(production);
-            productionTokens.emplace_back(Token(production, Terminal::NON_TERMINAL));
+            auto productionTerm = productionsToTerminals.find(production);
+
+            if (productionTerm != productionsToTerminals.end()) {
+                if (productionTerm->second == inputToken->term)
+                    productionTokens.push_back(*inputToken);
+                else
+                    productionTokens.emplace_back(production, productionTerm->second);
+            } else
+                productionTokens.emplace_back(production, Terminal::NON_TERMINAL);
         }
+
+    for (auto production = productionTokens.rbegin(); production != productionTokens.rend(); ++production) {
+        if (production->term == Terminal::NON_TERMINAL)
+            grammarSymbols.push(production->lexeme);
+        else
+            grammarSymbols.push(terminalsToProductions.find(production->term)->second);
+    }
+
     st.insert(currentGrammarSymbol, productionTokens);
 }
 
 void ccc::LL1Parser::continueGrammarMatching()
 {
     grammarSymbols.pop();
-    sharedBuffer.pop();
+    buffer.pop();
 }
 
-bool ccc::LL1Parser::parsingAlgorithm(SyntaxTree& res)
+bool ccc::LL1Parser::parse(SyntaxTree& res)
 {
     res.insert("", std::vector<Token> { Token("E", Terminal::NON_TERMINAL) });
     std::string currentGrammarSymbol = grammarSymbols.top();
 
     while (currentGrammarSymbol != std::string("$")) {
-        Terminal inputTerm = sharedBuffer.front()->term;
+        Terminal inputTerm = buffer.consume()->term;
         // terminal
-        if (currentGrammarSymbol == Token::terminalNames[inputTerm]) {
+        if (terminalsToProductions.find(inputTerm) == terminalsToProductions.end())
+            // error out
+            return false;
+        if (currentGrammarSymbol == terminalsToProductions.find(inputTerm)->second) {
             // if the currentGrammarSymbol is the same terminal as the input
             continueGrammarMatching();
         } else if (terminals.find(currentGrammarSymbol) != terminals.end()) {
@@ -239,4 +356,47 @@ bool ccc::LL1Parser::parsingAlgorithm(SyntaxTree& res)
         currentGrammarSymbol = grammarSymbols.top();
     }
     return true;
+}
+
+void ccc::LL1Parser::convertToAst(SyntaxTree& st)
+{
+    st.root = convertToAstHelper(st.root);
+}
+
+ccc::SyntaxTree::SyntaxTreeNode* ccc::LL1Parser::convertToAstHelper(SyntaxTree::SyntaxTreeNode* node)
+{
+    if (node == nullptr)
+        return node;
+    SyntaxTree::SyntaxTreeNode* next = convertToAstHelper(node->next);
+    SyntaxTree::SyntaxTreeNode* children = convertToAstHelper(node->children);
+    bool reorderChildren = false;
+
+    if (next != nullptr) {
+        if (children == nullptr)
+            node->children = next;
+        else {
+            SyntaxTree::SyntaxTreeNode* temp = children;
+            while (temp->next != nullptr)
+                temp = temp->next;
+            temp->next = next->children;
+            next->children = children;
+            node->children = next;
+            reorderChildren = true;
+        }
+    }
+    node->next = nullptr;
+
+    if (children == nullptr && next == nullptr) {
+        delete node->children;
+        node->children = nullptr;
+    }
+    if (node->val.term == Terminal::NON_TERMINAL) {
+        SyntaxTree::SyntaxTreeNode* temp = node->children;
+        node->children = nullptr;
+        delete node;
+        if (reorderChildren)
+            return temp;
+        return children;
+    }
+    return node;
 }
